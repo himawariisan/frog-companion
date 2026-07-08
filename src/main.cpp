927e954bb459b1b99742d628bd80633b67260376
Sofiia <sofarukomeda@gmail.com>
2026-07-08 22:19:07 +0300

    Add: Initialize display

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,46 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include "Frog.h"
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
 Frog frog;
+
+Adafruit_SSD1306 display(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    &Wire,
+    OLED_RESET
+);
 
 void setup()
 {
     Serial.begin(115200);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
+        while (true);
+    }
+
+    display.clearDisplay();
+      
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);  
+
+    display.display();
 }
 
 void loop()
 {
-    Serial.println(frog.getX());
+    display.clearDisplay();
+
+    display.setCursor(0, 0);
+    display.println("OLED works!");
+
+    display.display();
+
     delay(1000);
 }
